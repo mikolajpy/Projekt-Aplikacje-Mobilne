43da7e04512bf376ab06ef8ee1a0b2bea33dd0ed
BigGamingGamer <75428891+mikolajpy@users.noreply.github.com>
2024-03-24 11:04:33 +0100

    Delete getfrogs directory

--- getfrogs/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('', views.endpoints),
-    path('advocates/', views.advocate_list),
-    path('advocates/<str:username>/', views.advocate_detail), #username jest traktowany jako inentyfikator typu int => liczba
-]
