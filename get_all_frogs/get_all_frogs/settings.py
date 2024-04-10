6d43862f5c416b9033e8b9a7d9085733a5179791
Mikolaj Skwarek <264084@student.pwr.edu.pl>
2024-04-10 17:36:46 +0200

    graphical design
    All good, graphic design is my passion

--- get_all_frogs/get_all_frogs/settings.py
+++ get_all_frogs/get_all_frogs/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+STATIC_ROOT = BASE_DIR / 'projectfiles'
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles',
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
