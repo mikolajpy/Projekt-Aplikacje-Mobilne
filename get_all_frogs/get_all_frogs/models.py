96dd24467c731196c0e3cf2d83e82ffbdac963ea
przybyszewskitomasz <78273234+serekwiejski88@users.noreply.github.com>
2024-06-04 16:07:57 +0200

    models

--- get_all_frogs/get_all_frogs/models.py
+++ get_all_frogs/get_all_frogs/models.py
@@ -70,30 +70,3 @@ def assign_achievment(sender, instance, action, **kwargs):
 
     if achievement:  
         assigned_acievment, created = AssignedAcievments.objects.get_or_create(achievement_owner=user, achievment_id=achievement)
-
-
-# class Chat(models.Model):
-#     participant_1 = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'participant_1')
-#     participant_2 = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'participant_2')
-
-#     recent_message_owner = models.TextField(blank = True, null = True, default = '')
-#     recent_message = models.TextField(blank = True, null = True, default = '')
-
-#     created = models.DateTimeField(auto_now = True)
-#     updated = models.DateTimeField(auto_now_add = True)
-    
-#     class Meta:
-#         ordering = ['-created', '-updated']
-
-
-# class Message(models.Model):
-#     chat = models.ForeignKey(Chat, on_delete = models.CASCADE)
-#     owner = models.ForeignKey(User, on_delete = models.CASCADE)
-
-#     body = models.TextField(blank = False, null = False)
-
-#     created = models.DateField(auto_now = True)
-#     updated = models.DateTimeField(auto_now_add = True)
-
-#     class Meta:
-#         ordering = ['-created', '-updated']
